e6bf5978a9bccba1915b8656ede72f25b9e6af49
XPX <asteroid@127.40.176.60.broad.hz.zj.dynamic.163data.com.cn>
2013-08-15 10:25:06 +0800

    Initial Commit

---
+++ qlifetest/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  qlifetestAppController.h
+//  qlifetest
+//
+//  Created by XPX on 13-8-15.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
